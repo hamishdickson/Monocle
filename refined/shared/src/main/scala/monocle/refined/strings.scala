4f449e3044c3bcf14a730430a7097af68f0b4183
Hamish Dickson <hamish.dickson@gmail.com>
2017-01-02 11:14:25 +0000

    Add support for refined endsWith. Fixes #481

--- refined/shared/src/main/scala/monocle/refined/strings.scala
+++ refined/shared/src/main/scala/monocle/refined/strings.scala
@@ -1,7 +1,7 @@
 package monocle.refined
 
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.StartsWith
+import eu.timepit.refined.string.{StartsWith, EndsWith}
 import monocle._
 
 object strings extends StringsInstances
@@ -11,6 +11,12 @@ trait StringsInstances {
   def startsWith(prefix: String): Prism[String, StartsWithString[prefix.type]] = {
     Prism.partial[String, Refined[String, StartsWith[prefix.type]]] {
       case string if string.startsWith(prefix) => Refined.unsafeApply(string)
+    }{_.value}
+  }
+
+  def endsWith(suffix: String): Prism[String, EndsWithString[suffix.type]] = {
+    Prism.partial[String, Refined[String, EndsWith[suffix.type]]] {
+      case string if string.endsWith(suffix) => Refined.unsafeApply(string)
     }{_.value}
   }
 
--- test/shared/src/test/scala/monocle/refined/StringsSpec.scala
+++ test/shared/src/test/scala/monocle/refined/StringsSpec.scala
@@ -3,7 +3,7 @@ package monocle.refined
 import monocle.MonocleSuite
 import monocle.law.discipline.PrismTests
 import org.scalacheck.Cogen
-import eu.timepit.refined.scalacheck.string.startsWithArbitrary
+import eu.timepit.refined.scalacheck.string.{startsWithArbitrary, endsWithArbitrary}
 import eu.timepit.refined._
 
 
@@ -13,11 +13,14 @@ import scalaz.Equal
 class StringsSpec extends MonocleSuite {
 
   implicit val startsWithCoGen: Cogen[StartsWithString[W.`"hello"`.T]] = Cogen[String].contramap[StartsWithString[W.`"hello"`.T]](_.value)
-
   implicit val eqStartsWith: Equal[StartsWithString[W.`"hello"`.T]] = Equal.equalA[StartsWithString[W.`"hello"`.T]]
+
+  implicit val endsWithCoGen: Cogen[EndsWithString[W.`"bye"`.T]] = Cogen[String].contramap[EndsWithString[W.`"bye"`.T]](_.value)
+  implicit val eqEndsWith: Equal[EndsWithString[W.`"bye"`.T]] = Equal.equalA[EndsWithString[W.`"bye"`.T]]
 
   implicit val eqString: Equal[String] = Equal.equalA[String]
 
   checkAll("starts with", PrismTests(startsWith("hello")))
+  checkAll("ends with", PrismTests(endsWith("bye")))
 
 }
